b6ce7d7d2ea22c7a3e6d854d937f0e1f352e032e
mamulee <ghdah7@gmail.com>
2021-01-19 09:12:04 +0900

    address changed

--- src/server/Address.java
+++ src/server/Address.java
@@ -2,12 +2,11 @@ package server;
 
 public class Address {
 
-	//희광ip번호: 1.238.104.121
 
 
-	private static String dbAddress = "193.123.224.23";
+	private static String dbAddress = "localhost";
 	private static int dbPort = 1521;
-	private static String serverAddress = "1.238.104.121";
+	private static String serverAddress = "localhost";
 	private static int serverPort = 5001;
 	public static String getDbAddress() {
 		return dbAddress;
